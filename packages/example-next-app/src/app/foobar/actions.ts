1c09730b749893546b5fb674190e8c01dbeb7a8e
tmtmtoo <tmtmt2209@gmail.com>
2025-07-11 16:19:02 +0900

    chore: update example-next-app

---
+++ packages/example-next-app/src/app/foobar/actions.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export const foobar = async () => {
+  return "foobar";
+};
---
+++ packages/example-next-app/src/app/foobar/component.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useActionState, startTransition } from "react";
+
+export const Foobar = (props: { action: () => Promise<string> }) => {
+  const [state, send] = useActionState(props.action, "initial");
+  const handler = () => startTransition(() => send());
+
+  return (
+    <div>
+      <p>State: {state}</p>
+      <button onClick={handler}>Send Action</button>
+    </div>
+  );
+};
